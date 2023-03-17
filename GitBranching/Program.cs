11ed9b95d471aaff21bc4c37d192c1cc4e11f1f3
jota0s <joaosilvaa.work@gmail.com>
2023-03-17 14:06:39 +0000

    pre merge

---
+++ GitBranching/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GitBranching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Hello, World!");
+             Console.WriteLine("Hello again");
+             
+        }
+
+    }
+}
