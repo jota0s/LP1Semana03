9694dd5a28dcbb512f41d2b9746fc2e0af304a9a
jota0s <joaosilvaa.work@gmail.com>
2023-03-17 13:08:45 +0000

    create the array float for the matrix

--- ArraySums/Program.cs
+++ ArraySums/Program.cs
@@ -8,18 +8,33 @@ namespace ArraySums
     {
         static void Main(string[] args)
         {
-          //ask the user to put a number of horizontal lines  
-          Console.WriteLine("insert a number of horizontal lines ");
+            //ask the user to put a number of horizontal lines  
+            Console.WriteLine("insert a number of horizontal lines ");
 
-          //save the number in a variable int   
-          int horizontal = int.Parse(Console.ReadLine());
-          
-          //ask the user to put a number of vertical lines  
-          Console.WriteLine("insert a number of vertical lines ");
+            //save the number in a variable int   
+            int totalLinhas = int.Parse(Console.ReadLine());
+            
+            //ask the user to put a number of vertical lines  
+            Console.WriteLine("insert a number of vertical lines ");
 
-          //save the number in a variable int    
-          int vertical = int.Parse(Console.ReadLine());
+            //save the number in a variable int    
+            int totalColunas = int.Parse(Console.ReadLine());
 
+            //create a matrix with the array float 
+            float[,] matrix = new float[totalLinhas,totalColunas];
+
+            
+            for( int linha= 0; linha<totalLinhas; linha++) 
+            {
+                for( int coluna = 0; coluna<totalColunas; coluna++)
+                {
+                     Console.WriteLine($"insert a value to position  ");
+
+                } 
+               
+               
+                   
+            }   
 
 
 
