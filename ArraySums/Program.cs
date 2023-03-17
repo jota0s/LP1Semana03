02d04e289c4e2ea8b523b77e6ff58e1a7980a808
jota0s <joaosilvaa.work@gmail.com>
2023-03-17 11:32:00 +0000

    creates the inputs to ask the user for a number

---
+++ ArraySums/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ArraySums
+
+{  
+     
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+          //ask the user to put a number of horizontal lines  
+          Console.WriteLine("insert a number of horizontal lines ");
+
+          //save the number in a variable int   
+          int horizontal = int.Parse(Console.ReadLine());
+          
+          //ask the user to put a number of vertical lines  
+          Console.WriteLine("insert a number of vertical lines ");
+
+          //save the number in a variable int    
+          int vertical = int.Parse(Console.ReadLine());
+
+
+
+
+        }
+    }
+
+}
