dc5c7ec9abaf8337c8e651e7299356f5f1aa2637
jota0s <joaosilvaa.work@gmail.com>
2023-03-17 14:58:28 +0000

    create some floats for the soma linhas and somacolunas

--- ArraySums/Program.cs
+++ ArraySums/Program.cs
@@ -9,35 +9,60 @@ namespace ArraySums
         static void Main(string[] args)
         {
             //ask the user to put a number of horizontal lines  
-            Console.WriteLine("insert a number of horizontal lines ");
+            Console.Write("insert a number of horizontal lines: ");
 
             //save the number in a variable int   
             int totalLinhas = int.Parse(Console.ReadLine());
             
             //ask the user to put a number of vertical lines  
-            Console.WriteLine("insert a number of vertical lines ");
+            Console.Write("insert a number of vertical lines: ");
 
             //save the number in a variable int    
             int totalColunas = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine();
 
             //create a matrix with the array float 
             float[,] matrix = new float[totalLinhas,totalColunas];
+            float[] somaLinhas = new float[totalLinhas];
+            float[] somaColunas = new float[totalColunas];   
+            for (int linha = 0; linha < totalLinhas; linha++)
+            {
+                for (int coluna = 0; coluna < totalColunas; coluna++)
+                {
+                    Console.Write($"insert a value position for [{linha}, {coluna}]: ");
+                    int value = int.Parse(Console.ReadLine());
+                    matrix[linha,coluna] = value;
+                    somaColunas[coluna] += value;
+                    somaLinhas[linha] +=value;
+                }
+            }
+
+            // printa a matriz para o user
+            Console.WriteLine();    
+            for (int linha = 0; linha < totalLinhas; linha++)
+            {
+                for (int coluna = 0; coluna < totalColunas; coluna++)
+                {
+                    Console.Write($"{matrix[linha,coluna]} ");
+                }
+                Console.WriteLine();    
+            }
+
+            // printa a soma das coluna para o user
+            Console.WriteLine();  
+            for (int i = 0; i < totalColunas; i++)
+            {
+                Console.WriteLine($"soma da coluna {i}: {somaColunas[i]}");      
+            }
+
+                 Console.WriteLine();  
+            for (int i = 0; i < totalLinhas; i++)
+            {
+                Console.WriteLine($"soma da linha {i}: {somaLinhas[i]}");      
+            }
 
             
-            for( int linha= 0; linha<totalLinhas; linha++) 
-            {
-                for( int coluna = 0; coluna<totalColunas; coluna++)
-                {
-                     Console.WriteLine($"insert a value to position  ");
-
-                } 
-               
-               
-                   
-            }   
-
-
-
         }
     }
 
