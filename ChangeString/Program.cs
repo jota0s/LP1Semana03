a036113b507469e53c7a5c9b655b59a099a8d96b
jota0s <joaosilvaa.work@gmail.com>
2023-03-17 00:09:02 +0000

    create a program than reads a string and a char, and print the string without the char.

---
+++ ChangeString/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("write a string");
+          String r = Console.ReadLine();
+
+
+          Console.WriteLine("write a char");
+          Char b = Char.Parse(Console.ReadLine());
+
+          string finalR ="";
+
+
+          foreach (char p in r)
+
+
+
+          {
+             if (p!=b)
+             {
+              finalR += p;
+             }
+
+
+
+          }
+
+          Console.WriteLine(finalR);
+
+
+
+        
+
+
+
+
+        }
+    }
+}
