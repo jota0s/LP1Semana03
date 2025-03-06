4f7e6ecb16113b2835e1439a914e9b513a61733a
Joao Silva <joaosilvaa.work@gmail.com>
2025-03-06 16:13:26 +0000

    Add ChangeString

---
+++ ChangeString/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {   
+            string d;
+            char c;
+
+            Console.Write("Ctring: ");
+            d = Console.ReadLine();
+
+            Console.Write("Caráter: ");
+            c = Console.ReadKey().KeyChar;
+
+            string resultado = "";
+            foreach (char item in d)
+            {
+                if (item ==c)
+                {
+                    resultado += "x";
+                }
+
+                else 
+                {
+                    resultado += item;
+                }
+            }
+        
+            Console.WriteLine(c);
+            Console.Write("Será impresso: " + resultado);
+        }
+    }
+}
