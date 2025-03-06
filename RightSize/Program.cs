635c702e81d8f68bd5137e0a1ab3c258e1f18a45
Joao Silva <joaosilvaa.work@gmail.com>
2025-03-06 14:34:31 +0000

    Add project RightSize

---
+++ RightSize/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RightSize
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            foreach (string s in args)
+            
+                {
+                    if (s.Length < 8)
+                    {
+                        if (s.Length> 3)
+                        {
+                            Console.WriteLine(s);
+                        }
+                    }
+
+                    else 
+                    {
+                        Console.WriteLine("[EARLY STOP]");
+                        break;
+                    }
+
+                }
+
+        }
+    }
+}
