45c350861d8b3c296c55bffe173e0d10aff2eb0c
Joao Silva <joaosilvaa.work@gmail.com>
2025-03-06 20:28:29 +0000

    add finished ex Pow2Until and HeroPerks

--- HeroPerk/Program.cs
+++ HeroPerk/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using HeroPerks;
 
 namespace HeroPerk
 {
@@ -6,7 +7,49 @@ namespace HeroPerk
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Perks myPerks = Perks.none;
+
+            foreach (char c in args[0])
+            {
+                switch(c)
+                {
+                    case 'w':
+                        myPerks ^= Perks.WarpShift;
+                        Console.Write("WarpShift ");
+                        break;
+                    case 's':
+                        myPerks ^= Perks.Stealth;
+                        Console.Write("Stealth ");
+                        break;
+                    case 'a':
+                        myPerks ^= Perks.AutoHeal;
+                        Console.Write("AutoHeal ");
+                        break;
+                    case 'd':
+                        myPerks ^= Perks.DoubleJump;
+                        Console.Write("DoubleJump ");
+                        break;
+                    default:
+                        Console.WriteLine("!Unknown perk!");
+                        break;
+                }
+            }
+
+            if (myPerks == Perks.none)
+            {
+                Console.WriteLine("!No perks at all!");
+                return;
+            }
+
+            if((myPerks & Perks.Stealth) == Perks.Stealth && (myPerks & Perks.DoubleJump) == Perks.DoubleJump)
+            {
+                Console.WriteLine("\n!Silent Jumper!");
+            }
+            if((myPerks & Perks.AutoHeal) != Perks.AutoHeal)
+            {
+                Console.WriteLine("!Not gonna make it!");
+            } 
+
         }
     }
 }
--- Pow2Until/Program.cs
+++ Pow2Until/Program.cs
@@ -4,9 +4,20 @@ namespace Pow2Until
 {
     public class Program
     {
-        private static void Main(string[] args)
+        private static void PowersOf2Until5()
+    {
+        for (int i = 1; i <= (1 << 5); i = i << 1)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.WriteLine(i);
         }
+    }
+
+    static void Main(string[] args)
+    {
+        // Chamar o o método duas vezes
+        PowersOf2Until5();
+        PowersOf2Until5();
+    }
+
     }
 }
